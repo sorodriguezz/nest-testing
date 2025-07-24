62ef0998f2a3679e9d18a7095a6686524d05a605
sorodriguezz <s.rodriguezzapata01@gmail.com>
2025-07-23 23:06:31 -0400

    test: add unit tests for bootstrap function in main.ts

---
+++ src/main.spec.ts
@@ -0,0 +1,72 @@
+import { NestFactory } from '@nestjs/core';
+import { bootstrap } from './main';
+import { AppModule } from './app.module';
+
+jest.mock('@nestjs/core', () => ({
+  NestFactory: {
+    create: jest.fn().mockResolvedValue({
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    }),
+  },
+}));
+
+describe('Main.ts bootstrap', () => {
+  let mockApp: {
+    useGlobalPipes: jest.Mock;
+    setGlobalPrefix: jest.Mock;
+    listen: jest.Mock;
+  };
+
+  beforeEach(() => {
+    mockApp = {
+      useGlobalPipes: jest.fn(),
+      setGlobalPrefix: jest.fn(),
+      listen: jest.fn(),
+    };
+
+    (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+  });
+
+  it('shiuld create application ', async () => {
+    await bootstrap();
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+  });
+
+  it('should set global prefix', async () => {
+    await bootstrap();
+
+    expect(mockApp.setGlobalPrefix).toHaveBeenCalledWith('api');
+  });
+
+  it('should listen on port 3000 if env port not set', async () => {
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(3000);
+  });
+
+  it('should listen on env port', async () => {
+    process.env.PORT = '3000';
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(process.env.PORT);
+  });
+
+  it('should use global pipes', async () => {
+    await bootstrap();
+
+    expect(mockApp.useGlobalPipes).toHaveBeenCalledWith(
+      expect.objectContaining({
+        errorHttpStatusCode: 400,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        validatorOptions: expect.objectContaining({
+          whitelist: true,
+          forbidNonWhitelisted: true,
+        }),
+      }),
+    );
+  });
+});
